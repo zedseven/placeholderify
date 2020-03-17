afc35bd088c2bc9d860993786c737836bddf3779
Zacchary Dempsey-Plante <zacchary.dempseyplante@gmail.com>
2020-03-16 21:04:07 -0400

    Fixed several issues.

--- plchld.go
+++ plchld.go
@@ -1,4 +1,4 @@
-package placeholderify
+package main
 
 import (
 	"fmt"
@@ -15,41 +15,44 @@ import (
 const (
 	plcHldDirAdd = ".plcHld"
 	plcHldR      = 255
-	plcHldG      = 255
-	plcHldB      = 0
+	plcHldG      = 0
+	plcHldB      = 255
 	plcHldA      = 255
-
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("You have to specify a srcPath to a directory.")
+		fmt.Println("You have to specify a srcRoot to a directory.")
 		return
 	}
 
-	srcPath := os.Args[1]
+	srcRoot := os.Args[1]
 
 	// TODO: Add single-file functionality
-	if id, err := isDir(srcPath); err != nil {
-		fmt.Println("The provided srcPath does not exist.")
+	if id, err := isDir(srcRoot); err != nil {
+		fmt.Println("The provided srcRoot does not exist.")
 		return
 	} else if !id {
-		fmt.Println("The provided srcPath is not a directory.")
+		fmt.Println("The provided srcRoot is not a directory.")
 		return
 	}
 
-	plcHldPath := filepath.Join(filepath.Dir(srcPath), filepath.Base(srcPath) + plcHldDirAdd)
-	fmt.Println("Placeholder:", plcHldPath)
-	if err := os.MkdirAll(plcHldPath, os.ModePerm); err != nil {
+	plcHldRoot := filepath.Join(filepath.Dir(srcRoot), filepath.Base(srcRoot) + plcHldDirAdd)
+	fmt.Println("Placeholder:", plcHldRoot)
+	if err := os.MkdirAll(plcHldRoot, os.ModePerm); err != nil {
 		fmt.Println("Unable to make the placeholder directory.")
 		return
 	}
-	if err := filepath.Walk(srcPath, func(p string, i os.FileInfo, e error) error {
-		return plcHldify(srcPath, plcHldPath, p)
+	if err := filepath.Walk(srcRoot, func(p string, i os.FileInfo, e error) error {
+		return plcHldify(srcRoot, plcHldRoot, p)
 	}); err != nil {
-		fmt.Println("An error occurred while working on the srcPath.", err.Error())
+		fmt.Println("An error occurred while working on the srcRoot.", err.Error())
 		return
 	}
+
+	cullStumps(plcHldRoot)
+
+	fmt.Println("Done!")
 }
 
 func isDir(path string) (bool, error) {
@@ -83,7 +86,7 @@ func plcHldify(srcRoot, plcHldRoot, srcPath string) error {
 		if err != nil {
 			return err
 		}
-		ext := strings.ToLower(filepath.Ext(srcPath))
+		ext := strings.TrimLeft(strings.ToLower(filepath.Ext(srcPath)), ".")
 		switch ext {
 		case "jpg", "jpeg", "png":
 			iinfo, _, err := image.DecodeConfig(file)
@@ -91,10 +94,16 @@ func plcHldify(srcRoot, plcHldRoot, srcPath string) error {
 				return err
 			}
 			img := plcHldImg(iinfo.Width, iinfo.Height)
+			//fmt.Println(plcHldPath)
 			newFile, err := os.OpenFile(plcHldPath, os.O_RDWR | os.O_CREATE, os.ModePerm)
 			if err != nil {
 				return err
 			}
+			defer func() {
+				if err := newFile.Close(); err != nil {
+					fmt.Println("Error writing file \"", plcHldPath, "\":", err.Error())
+				}
+			}()
 			switch ext {
 			case "jpg", "jpeg":
 				if err := jpeg.Encode(newFile, img, &jpeg.Options{1}); err != nil {
@@ -118,4 +127,21 @@ func plcHldImg(width, height int) image.Image {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	draw.Draw(img, img.Bounds(), &image.Uniform{plcHldColour}, image.Pt(0, 0), draw.Src)
 	return img
+}
+
+func cull(path string, info os.FileInfo, err error) error {
+	// Removes if empty, otherwise throws an error
+	// Calls itself on the removed directory's parent, in case it just removed the last child
+	if id, err := isDir(path); err == nil && id {
+		if err := os.Remove(path); err == nil {
+			cull(filepath.Dir(path), nil, nil)
+		}
+	} else if err != nil {
+		return err
+	}
+	return nil
+}
+
+func cullStumps(root string) {
+	filepath.Walk(root, cull)
 }
